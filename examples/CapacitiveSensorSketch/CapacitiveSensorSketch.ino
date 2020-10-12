fe02e55fcddc12333a5a3a2d08568d3c38501794
Trey <tschisle@purdue.edu>
2020-10-12 18:48:03 -0400

    Final version of the capacitance test code file

--- examples/CapacitiveSensorSketch/CapacitiveSensorSketch.ino
+++ examples/CapacitiveSensorSketch/CapacitiveSensorSketch.ino
@@ -9,31 +9,32 @@
  */
 
 
-CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
-CapacitiveSensor   cs_4_6 = CapacitiveSensor(4,6);        // 10M resistor between pins 4 & 6, pin 6 is sensor pin, add a wire and or foil
-CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8);        // 10M resistor between pins 4 & 8, pin 8 is sensor pin, add a wire and or foil
+CapacitiveSensor   cs_4_2 = CapacitiveSensor(14,27);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+//CapacitiveSensor   cs_4_6 = CapacitiveSensor(14,6);        // 10M resistor between pins 4 & 6, pin 6 is sensor pin, add a wire and or foil
 
 void setup()                    
 {
-   cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
-   Serial.begin(9600);
+   //cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+   Serial.begin(115200); 
+   pinMode(13, OUTPUT);
+   digitalWrite(13, LOW);
 }
 
 void loop()                    
 {
-    long start = millis();
-    long total1 =  cs_4_2.capacitiveSensor(30);
-    long total2 =  cs_4_6.capacitiveSensor(30);
-    long total3 =  cs_4_8.capacitiveSensor(30);
+    long total1 =  cs_4_2.capacitiveSensorRaw(30);
 
-    Serial.print(millis() - start);        // check on performance in milliseconds
+    Serial.print("Test:");        // check on performance in milliseconds
     Serial.print("\t");                    // tab character for debug windown spacing
-
+    //Serial.println(touchRead(7));
+    Serial.println(map(total1, 3800, 25900, 0, 100));
+    /*
     Serial.print(total1);                  // print sensor output 1
     Serial.print("\t");
     Serial.print(total2);                  // print sensor output 2
     Serial.print("\t");
     Serial.println(total3);                // print sensor output 3
+    */
 
-    delay(10);                             // arbitrary delay to limit data to serial port 
+    delay(500);                             // arbitrary delay to limit data to serial port 
 }
